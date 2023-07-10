ae59c839ec17ae541d0fcad9d11949637cd40797
iakev <kirimikmwongera@gmail.com>
2023-07-10 09:02:51 +0000

    Registered local apps to the admin site

--- administration/admin.py
+++ administration/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
+from .models import Supplier, Customer, Business, Employee
 # Register your models here.
+admin.site.register(Supplier)
+admin.site.register(Customer)
+admin.site.register(Business)
+admin.site.register(Employee)
