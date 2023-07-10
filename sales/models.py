58d27da515ffe4d761fd00aacdd4837f6369395f
iakev <kirimikmwongera@gmail.com>
2023-07-10 09:10:12 +0000

    Added a through table for Product to Sales many to many relationship

--- sales/models.py
+++ sales/models.py
@@ -28,10 +28,46 @@ class PaymentMode(models.Model):
 
     mode = models.CharField(max_length=3, choices=PaymentMethod.choices)
     properties = models.JSONField()
+    
+    #TODO: mode is cash
+    # properties = {
+    #     till: {1: 0, 5: 0, 10:0, 20:0, 50:0, 100: 0},
+    #     issued_amount: int,
+    #     change_due: int,
+    # }
+
+    # mode is credit
+    # properties = {
+    #     customer_id: customer,
+    #     date_due
+    #     desctiption
+    # }
+    # properties = {
+
+    # }
+
 
     # Logic to populate properties according to mode and then save to database
 
+class ProductSales(models.Model):
+    """
+    Models the through Table of Product to Sales many to many
+    """
+    products = models.ForeignKey(Product, related_name="sales", on_delete=models.CASCADE)
+    sales = models.ForeignKey('Sales', related_name="products", on_delete=models.CASCADE)
+    quantity_sold = models.DecimalField(max_digits=10, decimal_places=2, default=0.00)
+    price_per_unit = models.DecimalField(max_digits=10, decimal_places=2, default=0.00)
+    is_wholesale = models.BooleanField(default=False)
+    price = models.DecimalField(max_digits=10, decimal_places=2, default=0.00)
+    tax_amount = models.DecimalField(max_digits=10, decimal_places=2,default=0.00)
+    receipt_label = models.CharField(max_length=5)
+    tax_rate = models.CharField(max_length=5)
+    business_name = models.CharField(max_length=255)
+    business_pin = models.CharField(max_length=255)
+    address = models.CharField(max_length=255)
+    # logic for populating price_per_unit according to is_wholesale
 
+    # add logic for designating receipt label from sales transactiontype and sales receipt type
 class Sales(models.Model):
     """
     Sales Model information
@@ -66,11 +102,15 @@ class Sales(models.Model):
     customer_id = models.ForeignKey(Customer, related_name='sales', on_delete=models.CASCADE, null= True, blank=True)
     business_id = models.OneToOneField(Business, related_name='sale', on_delete=models.CASCADE)
     payment_id = models.OneToOneField(PaymentMode, related_name='sale', on_delete=models.CASCADE)
+    cashier_id = models.ForeignKey(Employee, related_name='sales', on_delete=models.CASCADE)
+    products = models.ManyToManyField(Product, related_name='sales', on_delete=models.CASCADE, through=ProductSales)
     time_created = models.DateTimeField(auto_now=True)
-    time_paid = models.DateTimeField(null=True)
     sale_amount_with_tax = models.DecimalField(max_digits=10,decimal_places=2,default=0.00,blank=True)    
     tax_amount = models.DecimalField(max_digits=10, blank=True,default=0.00, decimal_places=2)
     receipt_type = models.CharField(max_length=2, choices = SalesReceiptType.choices)
     transaction_type = models.CharField(max_length=2, choices=TransactionProgress.choices)
     sale_status = models.CharField(max_length=3, choices=TransactionProgress.choices, null=True, blank=True)
-    cashier_id = models.ForeignKey(Employee, related_name='sales', on_delete=models.CASCADE)
+
+    class Meta:
+        verbose_name_plural = 'sales'
+        ordering = ['time_created']
