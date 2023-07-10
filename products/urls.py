f3486d46f8948333c62a0e764e7b8494770a47de
bill omole <omolebill01@gmail.com>
2023-07-10 23:30:46 +0200

    added the stock urls

--- products/urls.py
+++ products/urls.py
@@ -1,8 +1,10 @@
 from products.api.v1.viewsets import ProductViewSet
 from products.api.v1.viewsets import CategoryViewSet
+from products.api.v1.viewsets import StockViewSet
 from rest_framework.routers import DefaultRouter
 
 router = DefaultRouter()
 router.register(r'products', ProductViewSet, basename='product')
 router.register(r'categories', CategoryViewSet, basename='category')
+router.register(r'stocks', StockViewSet, basename='stock')
 urlpatterns = router.urls
