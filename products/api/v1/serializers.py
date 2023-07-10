bf0421f706dd3d4db38201c692e6d52c9af35fdd
iakev <kirimikmwongera@gmail.com>
2023-07-10 19:24:11 +0000

    added REST API folder and files for products app

---
+++ products/api/v1/serializers.py
@@ -0,0 +1,51 @@
+"""
+Module for creating serializers for Product application models
+"""
+from rest_framework import serializers
+
+from administration.models import Supplier
+from products.models import Category, Product, Stock, SupplierProduct
+
+class ProductSerializer(serializers.ModelSerializer):
+    """
+    Serializer for Product model
+    """
+    class Meta:
+        model = Product
+        fields = ['category', 'name', 'uuid', 'code', 
+                    'description', 'product_type', 'tax_type', 
+                    'unit', 'limited', 'active_for_sale']
+
+class CategorySerializer(serializers.ModelSerializer):
+    """
+    Serializer for Category model
+    """
+    class Meta:
+        model = Category
+        fields = ['name', 'uuid', 'image', 'thumbnail']
+
+class StockSerializer(serializers.ModelSerializer):
+    """
+    Serializer for Stock model
+    """
+    class Meta:
+        model = Stock
+        fields = ['product_id', 'uuid', 'stock_quantity', 
+                  'updated_at', 'cost_per_unit', 'price_per_unit_retail', 
+                  'price_per_unit_wholesale', 'reorder_level', 'reorder_quantinty' ]
+
+class SupplierSerializer(serializers.ModelSerializer):
+    """
+    Serializer for Supplier model
+    """
+    class Meta:
+        model = Supplier
+        fields = ['uuid', 'products', 'name', 'address', 'email_address', 'phone_number']
+
+class SupplierProductSerializer(serializers.ModelSerializer):
+    """
+    Serializer for SupplierProduct model
+    """
+    class Meta:
+        model = SupplierProduct
+        fields = ['supplier', 'product']
---
+++ products/api/v1/viewsets.py
@@ -0,0 +1,114 @@
+"""
+Module illustrating the viewsets for product API's
+"""
+from django.shortcuts import get_object_or_404
+from rest_framework.response import Response
+from rest_framework.viewsets import ViewSet
+
+from products.models import Product, Category
+from .serializers import ProductSerializer, CategorySerializer
+
+class CategoryViewSet(ViewSet):
+    """Basic viewset for Category Related Items"""
+    queryset = Category.objects.all()
+
+    def list(self, request, *args, **kwargs):
+        """Return a list of all categories"""
+        serializer = CategorySerializer(self.queryset, many=True)
+        return Response(serializer.data)
+    
+    def retrieve(self, request, pk=None):
+        """Return a single category"""
+        category = get_object_or_404(self.queryset, uuid=pk)
+        serializer = CategorySerializer(category)
+        return Response(serializer.data)
+    
+    def create(self, request, *args, **kwargs):
+        """Create a new category"""
+        serializer = CategorySerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=201)
+        return Response(serializer.errors, status=400)
+    
+    def update(self, request, pk=None):
+        """Update an existing category"""
+        category = get_object_or_404(self.queryset, uuid=pk)
+        serializer = CategorySerializer(category, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=400)
+    
+    def partial_update(self, request, pk=None):
+        """Update an existing category"""
+        category = get_object_or_404(self.queryset, uuid=pk)
+        serializer = CategorySerializer(category, data=request.data, partial=True)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=400)
+    
+    def destroy(self, request, pk=None):
+        """Delete an existing category"""
+        category = get_object_or_404(self.queryset, uuid=pk)
+        category.delete()
+        return Response(status=204)
+
+class ProductViewSet(ViewSet):
+    """Basic viewset for Product Related Items"""
+    product_queryset = Product.objects.all()
+    category_queryset = Category.objects.all()
+
+    def list(self, request, *args, **kwargs):
+        """Return a list of all products"""
+        serializer = ProductSerializer(self.product_queryset, many=True)
+        return Response(serializer.data)
+    
+    def retrieve(self, request, pk=None):
+        """Return a single product"""
+        product = get_object_or_404(self.product_queryset, uuid=pk)
+        serializer = ProductSerializer(product)
+        return Response(serializer.data)
+    
+    def create(self, request, *args, **kwargs):
+        """Create a new product"""
+        data = request.data
+        print(f"data is {data}")
+        uuid = data.get('category', None)
+        print(f"uuid is {uuid}")
+        if uuid:
+            print(uuid)
+            category = get_object_or_404(self.category_queryset, uuid=uuid)
+            print(f"the category is {category}")
+            data = request.data
+            data['category'] = category.id
+            serializer = ProductSerializer(data=data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=201)
+        return Response(serializer.errors, status=400)
+    
+    def update(self, request, pk=None):
+        """Update an existing product"""
+        product = get_object_or_404(self.product_queryset, uuid=pk)
+        serializer = ProductSerializer(product, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=400)
+    
+    def partial_update(self, request, pk=None):
+        """Update an existing product"""
+        product = get_object_or_404(self.product_queryset, uuid=pk)
+        serializer = ProductSerializer(product, data=request.data, partial=True)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=400)
+    
+    def destroy(self, request, pk=None):
+        """Delete an existing product"""
+        product = get_object_or_404(self.product_queryset, uuid=pk)
+        product.delete()
+        return Response(status=204)
