27c0efe59cf44d019d8c6a0d0bc2b3d96d60663a
iakev <kirimikmwongera@gmail.com>
2023-07-19 14:32:34 +0000

    Generated openapi based documentation

--- pos_inventory/users/urls.py
+++ pos_inventory/users/urls.py
@@ -3,17 +3,19 @@ from django.urls import path
 from pos_inventory.users.api.viewsets import UserViewSet
 from rest_framework.routers import DefaultRouter
 
-router = DefaultRouter()
-# from pos_inventory.users.views import (
-#     user_detail_view,
-#     user_redirect_view,
-#     user_update_view,
-# )
 
-# app_name = "users"
-# urlpatterns = [
-#     path("~redirect/", view=user_redirect_view, name="redirect"),
-#     path("~update/", view=user_update_view, name="update"),
-#     path("<str:username>/", view=user_detail_view, name="detail"),
-# ]
+from pos_inventory.users.views import (
+    user_detail_view,
+    user_redirect_view,
+    user_update_view,
+)
+
+app_name = "users"
+urlpatterns = [
+    path("~redirect/", view=user_redirect_view, name="redirect"),
+    path("~update/", view=user_update_view, name="update"),
+    path("<str:username>/", view=user_detail_view, name="detail"),
+]
+
+router = DefaultRouter()
 router.register(r"users", UserViewSet, basename="users")
