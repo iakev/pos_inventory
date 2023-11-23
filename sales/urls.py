8d1ef2a9998a2c01e4ccfe63b7c71b4325cfed2f
iakev <kirimikmwongera@gmail.com>
2023-11-23 22:17:45 +0000

    Modified app urls to relfect changes made by adding the viewset changed

--- sales/urls.py
+++ sales/urls.py
@@ -1,5 +1,9 @@
 from sales.api.v1.viewsets import SalesViewSet, CustomerViewset
-from sales.api.v1.viewsets import ProductSalesViewset, PaymentModeViewSet, PurchaseViewset
+from sales.api.v1.viewsets import (
+    ProductSalesViewset,
+    PaymentModeViewSet,
+    PurchaseViewSet,
+)
 
 from rest_framework.routers import DefaultRouter
 
@@ -8,6 +12,6 @@ router.register(r"sales", SalesViewSet, basename="sales")
 router.register(r"productsales", ProductSalesViewset, basename="productsales")
 router.register(r"customers", CustomerViewset, basename="customers")
 router.register(r"paymentmodes", PaymentModeViewSet, basename="paymentmodes")
-router.register(r"purchases", PurchaseViewset, basename="purchases")
+router.register(r"purchases", PurchaseViewSet, basename="purchases")
 
 urlpatterns = router.urls
