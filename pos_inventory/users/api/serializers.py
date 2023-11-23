0bbde1969c9e49380bb1ba68261bd1ca800d3f89
iakev <kirimikmwongera@gmail.com>
2023-11-23 21:44:40 +0000

    Modified and added Serializer to ensure that serializers are correctly defined for both the request data and response data and embdeded in other serializers for User model

--- pos_inventory/users/api/serializers.py
+++ pos_inventory/users/api/serializers.py
@@ -1,3 +1,4 @@
+from typing import Any
 from django.contrib.auth import get_user_model
 from rest_framework import serializers
 
@@ -5,13 +6,13 @@ User = get_user_model()
 
 
 class UserSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True)
+
     class Meta:
         model = User
-        fields = ["uuid", "username", "first_name", "last_name"]
+        fields = ["uuid", "username", "password", "first_name", "last_name"]
         extra_kwargs = {
             "username": {"required": True},
-            "first_name": {"required": False},
-            "last_name": {"required": False},
         }
 
     def create(self, validated_data):
@@ -28,17 +29,30 @@ class UserSerializer(serializers.ModelSerializer):
         """
         Updates a user instace
         """
-        print("we are updating a user instance")
         username = validated_data.get("username")
         if "username" in validated_data:
             username = validated_data["username"]
             if User.objects.filter(username=username).exclude(pk=user.id).exists():
-                raise serializers.ValidationError(
-                    "User with this username already exists."
-                )
+                raise serializers.ValidationError("User with this username already exists.")
             user.username = username
         user.first_name = validated_data.get("first_name", user.first_name)
         user.last_name = validated_data.get("last_name", user.last_name)
-        user.set_password(validated_data.get("password", user.password))
+        user.set_password(validated_data.get("password"), user.password)
         user.save()
         return user
+
+    def to_representation(self, instance):
+        """
+        Delegating response to appropriate serializer
+        """
+        return UserResponseSerializer(context=self.context).to_representation(instance)
+
+
+class UserResponseSerializer(serializers.ModelSerializer):
+    """
+    Response Serializer for User data
+    """
+
+    class Meta:
+        model = User
+        fields = ["uuid", "username", "first_name", "last_name"]
