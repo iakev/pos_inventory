52f23c194427418a3ba1b29742cb08416c4ebcce
iakev <kirimikmwongera@gmail.com>
2023-11-23 21:33:58 +0000

    Created an ownerViewset and registered it in the urls configuration and router

--- administration/urls.py
+++ administration/urls.py
@@ -1,10 +1,12 @@
-from administration.api.v1.viewsets import (
-    BusinessViewset,
-    EmployeeViewset,
-)
-from rest_framework.routers import DefaultRouter
+# from administration.api.v1.viewsets import (
+#     BusinessViewset,
+#     EmployeeViewset,
+#     OwnerViewset,
+# )
+# from rest_framework.routers import DefaultRouter
 
-router = DefaultRouter()
-router.register(r"business", BusinessViewset, basename="business")
-router.register(r"employee", EmployeeViewset, basename="employee")
-urlpatterns = router.urls
+# router = DefaultRouter()
+# router.register(r"business", BusinessViewset, basename="business")
+# router.register(r"employee", EmployeeViewset, basename="employee")
+# router.register(r"owners", OwnerViewset, basename="owners")
+# urlpatterns = router.urls
