719193ce00cf68bfd757d1bd6410abadc1fe8d8f
iakev <kirimikmwongera@gmail.com>
2023-11-23 21:24:50 +0000

    Modified and added Serializer to ensure that serializers are correctly defined for both the request data and response data and embdeded in other serializers for Owner and Employee models.

--- administration/api/v1/serializers.py
+++ administration/api/v1/serializers.py
@@ -2,18 +2,18 @@
 Module for serializing Administrations
 """
 from django.contrib.auth import get_user_model
-from typing import Any
+from django.shortcuts import get_object_or_404
+from typing import Any, Dict
 from rest_framework import serializers
-from administration.models import Business, Employee
-from pos_inventory.users.api.serializers import UserSerializer
+from administration.models import Business, Employee, Owner, Supplier
+from products.models import SupplierProduct
+from pos_inventory.users.api.serializers import UserSerializer, UserResponseSerializer
 
 User = get_user_model()
 
 
 class BusinessSerializer(serializers.ModelSerializer):
     """Serializer for Business model"""
-
-    owner = UserSerializer(required=False)  # fro partial update
 
     class Meta:
         model = Business
@@ -26,48 +26,80 @@ class BusinessSerializer(serializers.ModelSerializer):
             "tax_pin",
             "phone_number",
             "email_address",
-            "owner",
         ]
 
-    def create(self, validated_data):
-        """
-        Overwriting this method inorder to deal with
-        the nested User instance creation
-        """
-        owner_data = validated_data.pop("owner")
-        owner_serializer = UserSerializer(data=owner_data)
-        if owner_serializer.is_valid():
-            owner = owner_serializer.save()
-            validated_data["owner"] = owner
-            return super().create(validated_data)
-        raise serializers.ValidationError(owner_serializer.errors)
 
-    def update(self, business, validated_data):
+class OwnerSerializer(serializers.ModelSerializer):
+    """
+    Serializer for the owner model
+    """
+
+    user_uuid = serializers.UUIDField()
+    business_uuid = serializers.UUIDField()
+
+    class Meta:
+        model = Owner
+        fields = [
+            "uuid",
+            "business_uuid",
+            "user_uuid",
+        ]
+
+    def validate(self, data):
         """
-        Nested fields update method not handled by default
-        We never update the user instance except when we need to then we are creating
-        a new user
+        Custom validation mapping user_uuid and business_uuid
+        to their respective objects
         """
-        owner_data = validated_data.pop("owner", None)
-        if owner_data:
-            owner = business.owner
-            owner_serializer = UserSerializer(data=owner_data)
-            if owner_serializer.is_valid():
-                owner = owner_serializer.update(owner, owner_serializer.validated_data)
-                business.owner = owner
-            else:
-                raise serializers.ValidationError(owner_serializer.errors)
-        business.name = validated_data.get("name", business.name)
-        business.address = validated_data.get("address", business.address)
-        business.tax_pin = validated_data.get("tax_pin", business.tax_pin)
-        business.phone_number = validated_data.get(
-            "phone_number", business.phone_number
-        )
-        business.email_address = validated_data.get(
-            "email_address", business.email_address
-        )
-        business.save()
-        return business
+        user_uuid = data.pop("user_uuid", None)
+        business_uuid = data.pop("business_uuid", None)
+        validated_data = super().validate(data)
+        if user_uuid:
+            print(f"we have user_uuid as {user_uuid}")
+            user = get_object_or_404(User, uuid=user_uuid)
+            print(f"we have tje user object as {user}")
+            validated_data["user"] = user
+        if business_uuid:
+            business = get_object_or_404(Business, uuid=business_uuid)
+            validated_data["business"] = business
+        return validated_data
+
+    # def create(self, validated_data):
+    #     """
+    #     Overwriting this method to handle the owner nested creation
+    #     NB: Since owner is a nested field
+    #     """
+
+    def update(self, owner, validated_data):
+        """
+        Updates Nested Owner data
+        """
+        user = validated_data.get("user", None)
+        business = validated_data.get("business", None)
+        if user:
+            owner.user = user
+        if business:
+            owner.business = business
+        owner.save()
+        return owner
+
+    def to_representation(self, instance):
+        """
+        Delegating representation to appropriate serializer
+        """
+        return OwnerResponseSerializer(context=self.context).to_representation(instance)
+
+
+class OwnerResponseSerializer(serializers.ModelSerializer):
+    """
+    Custom Response data serializer for owner
+    """
+
+    user = UserResponseSerializer()
+    business = BusinessSerializer(required=False)
+
+    class Meta:
+        model = Owner
+        fields = ["uuid", "user", "business"]
 
 
 class EmployeeSerializer(serializers.ModelSerializer):
@@ -109,7 +141,6 @@ class EmployeeSerializer(serializers.ModelSerializer):
         user_data = validated_data.pop("user", None)
         if user_data:
             user = employee.user
-            print(f"we have the user as {user}")
             user_serializer = UserSerializer(user, data=user_data, partial=True)
             if user_serializer.is_valid():
                 user = user_serializer.update(user, user_serializer.validated_data)
@@ -118,11 +149,23 @@ class EmployeeSerializer(serializers.ModelSerializer):
                 raise serializers.ValidationError(user_serializer.errors)
         employee.address = validated_data.get("address", employee.address)
         employee.department = validated_data.get("department", employee.department)
-        employee.phone_number = validated_data.get(
-            "phone_number", employee.phone_number
-        )
-        employee.email_address = validated_data.get(
-            "email_address", employee.email_address
-        )
+        employee.phone_number = validated_data.get("phone_number", employee.phone_number)
+        employee.email_address = validated_data.get("email_address", employee.email_address)
         employee.save()
         return employee
+
+
+class EmployeeRestrictedSerializer(serializers.ModelSerializer):
+    """
+    Custom serializer for employee serializer Response in ather viewsets
+    """
+
+    user = UserResponseSerializer()
+
+    class Meta:
+        model = Employee
+        fields = ["user", "department"]
+
+
+class AdministrationNotFoundSerializer(serializers.Serializer):
+    detail = serializers.CharField(default="Unfortunately requested resource not found")
